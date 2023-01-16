3228ffba1cb23db89114a676676b54956857b355
pureum <lpm0831@naver.com>
2023-01-16 11:33:19 +0900

    new

---
+++ app/src/main/java/retrofit_DI/example/retrofit_practice/domain/DomainData.kt
@@ -0,0 +1,11 @@
+package retrofit_DI.example.retrofit_practice.domain
+
+import retrofit_DI.example.retrofit_practice.data.Data_Detail
+
+/**
+ * 2023-01-15
+ * pureum
+ */
+data class DomainData(
+    val list: List<Data_Detail>?
+)
